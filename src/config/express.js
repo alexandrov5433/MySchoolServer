816c47d60d1a54c73fd080ad25b6858eaa18b507
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-01 17:29:47 +0100

    Comment added.

--- src/config/express.js
+++ src/config/express.js
@@ -5,6 +5,7 @@ import { checkCookie } from "../service/userSession.js";
 function configExpress(app) {
     app.use(urlencoded({extended: false}));
     app.use('/static', static_('static'));
+    //TODO is static needed??? if not delete
     app.use(cookieParser());
     app.use(checkCookie);
 }
