e3bcdd996a6b9f1872c8cf662b7a24c3b5b33b84
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-02 15:51:16 +0100

    __basedir variable added.

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 export const jwtSecret = process.env.JWT_SECRET || 'thisSecretShouldBeStoredInA.ENVFile';
 export const authCookieName = process.env.AUTH_COOKIE_NAME || 'mySchoolAuthCookie';
 export const port = process.env.PORT || 3000;
@@ -5,3 +7,4 @@ export const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/mySchoolPr
 export const bcryptSaltRounds = process.env.BCRYPT_SALT_ROUNDS || 9;
 export const teacherAuthCode = process.env.TEACHER_AUTH_CODE || '123';
 export const origin = process.env.ORIGIN.split(',') || ['http://localhost:5555', 'http://localhost:4200'];
+export const __basedir = path.normalize(import.meta.dirname + '/../../');
