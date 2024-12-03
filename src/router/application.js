441d79d9740eccecd45714c642fb389022b77be7
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-03 17:04:26 +0100

    ApplyNow handler added.

--- src/router/application.js
+++ src/router/application.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { application } from "../controllers/application.js";
 
 const router = Router();
 
 // router.get('/:applicationId', );  //teacher application review
-// router.post('/', ); //apply now (register) - applicant (student)
+router.post('/', application.apply); //apply now (register) - applicant (student)
 // router.delete('/:applicationId', );  //teacher REJECTS application
 // router.put('/:applicationId', );  //teacher ACCEPTS application
 
