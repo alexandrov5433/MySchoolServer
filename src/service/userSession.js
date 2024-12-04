5c710768f0e8b2bd97c8d6aae5218e6b33334dea
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 09:37:09 +0100

    JWT cookie parsing made async.

--- src/service/userSession.js
+++ src/service/userSession.js
@@ -1,21 +1,29 @@
 import jwt from 'jsonwebtoken';
 import { jwtSecret } from '../config/serverConfig.js';
 
-function checkCookie(req, res, next) {
+async function checkCookie(req, res, next) {
     if (req.cookies.user) {
-        jwt.verify(req.cookies.user, jwtSecret, (err, token) => {
-            if (err) {
-                console.log(err);
-                req.userSession = false;
-                res.locals.userSession = false;
-                next();
-            } else {
-                req.userSession = true;
-                res.locals.userSession = true;
-                req.cookies.user = token;
-                next();
-            }
-        });
+        try {
+            const result = new Promise((resolve, reject) => {
+                jwt.verify(req.cookies.user, jwtSecret, (err, token) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(token);
+                    }
+                });
+            });
+            const token = await result;
+            req.userSession = true;
+            res.locals.userSession = true;
+            req.cookies.user = token;
+            next();
+        } catch (e) {
+            console.error(e);
+            req.userSession = false;
+            res.locals.userSession = false;
+            next();
+        }
     } else {
         req.userSession = false;
         res.locals.userSession = false;
