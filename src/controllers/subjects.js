522198b481e2775758880e3dccd43b54678e72a0
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 22:41:43 +0100

    Subjects queries and handler added.

--- src/controllers/subjects.js
+++ src/controllers/subjects.js
@@ -10,7 +10,7 @@ async function createNewSubject(req, res) {
         }
         const displayId = genDisplayId();
         const teacher = req.cookies?.user?._id;
-        const newSubject = await subjectService.createNewSubject({teacher, title, displayId});
+        const newSubject = await subjectService.createNewSubject({ teacher, title, displayId });
         res.status(200);
         res.json(JSON.stringify({
             status: 200,
@@ -27,9 +27,65 @@ async function createNewSubject(req, res) {
         }));
         res.end();
     }
-    
+}
+
+async function getSubjects(req, res) {
+    try {
+        const _id = req.cookies?.user?._id;
+        const title = req.query.title;
+        const subjectDisplayId = req.query.subjectDisplayId;
+        const mySubjectsOnly = req.query.mySubjectsOnly;
+        const payload = {
+            results: []
+        };
+        let searchRes;
+        if (mySubjectsOnly === 'true') {
+            if (req.cookies?.user?.status === 'student') {
+                searchRes = await subjectService.getSubjectsForParticipant(_id);
+            } else if (req.cookies?.user?.status === 'teacher') {
+                searchRes = await subjectService.getSubjectsForTeacher(_id);
+            }
+        } else {
+            searchRes = await subjectService.getSubjectsByTitleAndDisplayId(title, subjectDisplayId);
+        }
+        // console.log(searchRes);
+        // payload
+        // {
+        //     results: [
+        //         {
+        //             _id: string,
+        //             teacher: string,
+        //             title: string,
+        //             displayId: string,
+        //             backgroundImageNumber: string
+        //         }
+        //     ]
+        // }
+        payload.results = searchRes.reduce((acc, cur) => {
+            acc.push({
+                _id: cur._id,
+                teacher: cur.teacher.firstName + ' ' + cur.teacher.lastName,
+                title: cur.title,
+                displayId: cur.displayId,
+                backgroundImageNumber: cur.backgroundImageNumber,
+            });
+            return acc;
+        }, []);
+        res.status(200);
+        res.json(JSON.stringify(payload));
+        res.end();
+    } catch (e) {
+        console.log(e);
+        res.status(400);
+        res.json(JSON.stringify({
+            status: 400,
+            msg: parseError(e).errors
+        }));
+        res.end();
+    }
 }
 
 export const subjects = {
-    createNewSubject
+    createNewSubject,
+    getSubjects
 };
--- src/service/subject.js
+++ src/service/subject.js
@@ -3,7 +3,42 @@ import Subject from "../models/Subject.js";
 async function createNewSubject(data) {
     return await Subject.create(data);
 }
+/**
+ * Get the Subject documents in which the user with _id is a participant.
+ * @param {string} _id
+ * This is the _id of the User document in the DB. 
+ * @returns 
+ */
+async function getSubjectsForParticipant(_id) {
+    return await Subject.find({ participants: { $elemMatch: _id }}).populate('teacher');
+}
+
+async function getSubjectsByTitleAndDisplayId(title, displayId) {
+    const titleRegex = new RegExp(`${title}`);
+    const displayIdRegex = new RegExp(`${displayId}`);
+    return await Subject.find({ 
+        title: { $regex: titleRegex, $options: 'i' },
+        displayId: { $regex: displayIdRegex, $options: 'i' }
+    }).populate('teacher');
+}
+
+async function getSubjectById(_id) {
+    return await Subject.findById(_id);
+}
+/**
+ * Get the Subject documents in which the user with _id is a teacher (creator).
+ * @param {string} _id
+ * This is the _id of the User document in the DB. 
+ * @returns 
+ */
+async function getSubjectsForTeacher(_id) {
+    return await Subject.find({ teacher: _id }).populate('teacher');
+}
 
 export const subjectService = {
-    createNewSubject
+    createNewSubject,
+    getSubjectsForParticipant,
+    getSubjectsByTitleAndDisplayId,
+    getSubjectById,
+    getSubjectsForTeacher
 };
