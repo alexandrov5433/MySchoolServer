1da3cfb514576d50c342ba2bd8b72dcf8a8af99f
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 22:39:11 +0100

    Route guard added.

--- src/router/subjects.js
+++ src/router/subjects.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { subjects } from "../controllers/subjects.js";
+import { authenticationSrvice } from "../service/authentication.js";
 
 const router = Router();
 
 router.post('/create-new-subject', subjects.createNewSubject);
+router.get('/',
+    authenticationSrvice.authGuard('specificUserStatus', ['student', 'teacher']),
+    subjects.getSubjects
+);
 
 export default router;
