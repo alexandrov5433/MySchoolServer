ebc5ea3bfdc08e06c452ac9cfdfa0fd8857ab737
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 09:36:45 +0100

    Create new subject rute added.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-// import { authentificationMonitor as monitor } from "../service/userSession.js";
 import userRouter from './user.js';
 import applicationRouter from './application.js';
+import subjectsRouter from './subjects.js';
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/application', applicationRouter);
-
-
+router.use('/subjects', subjectsRouter);
 
 export default router;
