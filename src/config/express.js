5d7bc87710a39ddc24891e7f18c6d6c64599aa87
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 09:35:41 +0100

    JSON body parsing added.

--- src/config/express.js
+++ src/config/express.js
@@ -1,14 +1,15 @@
-import { urlencoded, static as static_ } from "express";
+import { urlencoded, static as static_, json } from "express";
 import cookieParser from "cookie-parser";
 import { checkCookie } from "../service/userSession.js";
 import fileUpload from "express-fileupload";
 
 function configExpress(app) {
     app.use(urlencoded({extended: false}));
+    app.use(json());
     app.use('/static', static_('static'));
     //TODO is static needed??? if not delete
     app.use(cookieParser());
-    // app.use(checkCookie);
+    app.use(checkCookie);
     app.use(fileUpload());
 }
 
