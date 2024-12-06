ccefc1ab127ddd12e54490424207ce4bb0377af7
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-06 22:32:11 +0100

    Syntax misstake corrected.

--- src/service/subject.js
+++ src/service/subject.js
@@ -10,20 +10,20 @@ async function createNewSubject(data) {
  * @returns 
  */
 async function getSubjectsForParticipant(_id) {
-    return await Subject.find({ participants: { $elemMatch: _id }}).populate('teacher');
+    return await Subject.find({ participants: { $elemMatch: { _id } } }).populate('teacher');
 }
 
 async function getSubjectsByTitleAndDisplayId(title, displayId) {
     const titleRegex = new RegExp(`${title}`);
     const displayIdRegex = new RegExp(`${displayId}`);
-    return await Subject.find({ 
+    return await Subject.find({
         title: { $regex: titleRegex, $options: 'i' },
         displayId: { $regex: displayIdRegex, $options: 'i' }
     }).populate('teacher');
 }
 
 async function getSubjectById(_id) {
-    return await Subject.findById(_id).populate('teacher');
+    return await Subject.findById(_id).populate(['teacher', 'materials', 'participants', 'assignments', 'announcements']);
 }
 /**
  * Get the Subject documents in which the user with _id is a teacher (creator).
@@ -35,10 +35,24 @@ async function getSubjectsForTeacher(_id) {
     return await Subject.find({ teacher: _id }).populate('teacher');
 }
 
+async function addParticipant(subjectId, userId) {
+    const subject = await Subject.findById(subjectId);
+    subject.participants.push(userId);
+    return subject.save();
+}
+
+async function removeParticipant(subjectId, userId) {
+    const subject = await Subject.findById(subjectId);
+    subject.participants = subject.participants.filter(p => p._id != userId);
+    return subject.save();
+}
+
 export const subjectService = {
     createNewSubject,
     getSubjectsForParticipant,
     getSubjectsByTitleAndDisplayId,
     getSubjectById,
-    getSubjectsForTeacher
+    getSubjectsForTeacher,
+    addParticipant,
+    removeParticipant
 };
