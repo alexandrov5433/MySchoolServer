25c36c47e70a39fac3a95376c35a1fc4de82a5e0
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-09 19:04:11 +0100

    Import rename.

--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,13 @@
 import { urlencoded, json } from "express";
 import cookieParser from "cookie-parser";
-import { authenticationSrvice } from '../service/authentication.js';
+import { authenticationService } from '../service/authentication.js';
 import fileUpload from "express-fileupload";
 
 function configExpress(app) {
     app.use(urlencoded({extended: false}));
     app.use(json());
     app.use(cookieParser());
-    app.use(authenticationSrvice.checkCookie);
+    app.use(authenticationService.checkCookie);
     app.use(fileUpload());
 }
 
