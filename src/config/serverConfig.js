579df5798743bde76c68a8ade6cf33146fd8b2c9
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-12 09:36:17 +0100

    Split func removed for origin variable. Server craches when there is no ORIGIN var in an .ENV file.

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -8,5 +8,5 @@ export const port = process.env.PORT || 3000;
 export const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/mySchoolProject';
 export const bcryptSaltRounds = process.env.BCRYPT_SALT_ROUNDS || 9;
 export const teacherAuthCode = process.env.TEACHER_AUTH_CODE || '123';
-export const origin = process.env.ORIGIN.split(',') || ['http://localhost:5555', 'http://localhost:4200'];
+export const origin = process.env.ORIGIN || ['http://localhost:5555', 'http://localhost:4200'];
 export const __basedir = path.normalize(import.meta.dirname + '/../../');
